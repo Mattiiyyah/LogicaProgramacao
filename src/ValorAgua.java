37647d5c8aa22f0201328175bf64fd29a6659b14
Matheus de Paula Santos <matheus1516paula@gmail.com>
2025-09-08 08:17:48 -0300

    correção do exercício

--- src/ValorAgua.java
+++ src/ValorAgua.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class ValorAgua {
    public static void main(String args[]) {
        Scanner leitor = new Scanner(System.in);
-       final double valor1 = 1.20, valor2 = 1.50, valor3 = 2.00, valor4 = 2.50;
+       final double valor1 = 1.20, valor2 = 1.50;
        double quan_Consumida, valorConsumido;
        System.out.print("\nDigite o valor de água consumido em metros cubicos: ");
        quan_Consumida = leitor.nextDouble();
@@ -11,17 +11,10 @@ public class ValorAgua {
        if (quan_Consumida > 0 && quan_Consumida <= 10) {
           valorConsumido = quan_Consumida * valor1;
           System.out.println("\nO valor consumido em metros cubicos é: " + valorConsumido);
-       } else if (quan_Consumida > 11 && quan_Consumida < 20) {
+       } else {
            valorConsumido = quan_Consumida * valor2;
            System.out.println("\nO valor consumido em metros cubicos é: " + valorConsumido);
-       } else if (quan_Consumida > 20 && quan_Consumida < 30) {
-           valorConsumido = quan_Consumida * valor3;
-           System.out.println("\nO valor consumido em metros cubicos é: " + valorConsumido);
-       } else {
-           valorConsumido = quan_Consumida * valor4;
-           System.out.println("\nO valor consumido em metros cubicos é: " + valorConsumido);
        }
-
        leitor.close();
        System.exit(0);
 
