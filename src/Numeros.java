33958d90d167ca0c906672f637ee3151643fd021
Matheus de Paula Santos <matheus1516paula@gmail.com>
2025-09-12 22:05:27 -0300

    Create Numeros.java

---
+++ src/Numeros.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Numeros {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        int numero;
+        System.out.println("\n --- ##VERIFICANDNO NÚMERO## ---");
+        System.out.print("\n Digite um número qualquer: ");
+        numero = leitor.nextInt();
+
+        if(numero > 0) {
+            System.out.print("\n O número: " + numero + " é positivo");
+        } else if (numero < 0 ) {
+            System.out.print("\n O número: " + numero + " é negativo");
+        } else {
+            System.out.print("\n O número: " + numero + " é zero");
+        }
+    }
+}
