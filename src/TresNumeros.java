b186b2b1b3e65204b3a8728a3b612809f14cc4fc
Matheus de Paula Santos <matheus1516paula@gmail.com>
2025-09-13 13:46:30 -0300

    Update TresNumeros.java

--- src/TresNumeros.java
+++ src/TresNumeros.java
@@ -3,17 +3,22 @@ public class TresNumeros {
     public static void main(String[] args) {
         Scanner leitor = new Scanner(System.in);
         double n1, n2, n3;
-        System.out.println("\n--- ## Exibindo maior número: ## ---");
-        System.out.print("\nDigite um número qualquer: ");
-        n1 = leitor.nextDouble();
-        System.out.print("\nDigite outro número qualquer: ");
-        n2 = leitor.nextDouble();
-        System.out.print("\nDigite outro número qualquer: ");
-        n3 = leitor.nextDouble();
 
-        if(n1 == n2 || n2 == n3 || n3 == n1) {
-            System.out.print("\nOs números não podem ser iguais.");
-        } else if(n1 > n2 && n1 > n3) {
+        do {
+            System.out.print("\nDigite o primeiro número: ");
+            n1 = leitor.nextDouble();
+            System.out.print("\nDigite o segundo número: ");
+            n2 = leitor.nextDouble();
+            System.out.print("\nDigite o terceiro número: ");
+            n3 = leitor.nextDouble();
+
+            if (n1 == n2 || n2 == n3 || n1 == n3) {
+                System.out.println("\n-- ERRO: Números iguais não são permitidos. Tente novamente. --");
+            }
+
+        } while (n1 == n2 || n2 == n3 || n1 == n3);
+
+        if(n1 > n2 && n1 > n3) {
             System.out.print("\nO maior número é: " + n1);
         } else if(n2 > n3 && n2 > n1) {
             System.out.print("\nO maior número é: " + n2);
