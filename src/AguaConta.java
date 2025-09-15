f5114edf5388cc9de67e80b0de59185e0408e053
Matheus de Paula Santos <matheus1516paula@gmail.com>
2025-09-15 19:43:22 -0300

    Update AguaConta.java

--- src/AguaConta.java
+++ src/AguaConta.java
@@ -2,25 +2,19 @@ import java.util.Scanner;
 public class AguaConta {
     public static void main(String[] args) {
         Scanner leitor = new Scanner(System.in);
-        final double v1 = 1.20, v2 = 1.50, v3 = 2.00, v4 = 2.50;
+        final int LIMITE1 = 10, LIMITE2 = 20, LIMITE3 = 30;
+        final double V1= 1.20, V2 = 1.50, V3 = 2.00, V4 = 2.50;
         double consumo, total;
         System.out.print("\n--- ## CALCULANDO VALOR DE ÁGUA ## ---");
         System.out.print("\nDigite a quantidade de água consumida em metros cúbicos: ");
         consumo = leitor.nextDouble();
 
-        if(consumo > 0 && consumo <= 10) {
-            total = consumo * v1;
-            System.out.printf("\nO total para pagar é: %.2f", total);
-        } else if(consumo > 10 && consumo <= 20) {
-            total = consumo * v2;
-            System.out.printf("\nO total para pagar é: %.2f", total);
-        } else if(consumo > 20 && consumo <= 30) {
-            total = consumo * v3;
-            System.out.printf("\nO total para pagar é: %.2f", total);
-        } else {
-            total = consumo * v4;
-            System.out.printf("\nO total para pagar é: %.2f", total);
-        }
+        if(consumo <= LIMITE1) total = consumo * V1;
+        else if(consumo <= LIMITE2) total = consumo * V2;
+        else if(consumo <= LIMITE3) total = consumo * V3;
+        else total = consumo * V4;
+
+        System.out.printf("\nO total para pagar é: %.2f", total);
 
         leitor.close();
         System.exit(0);
