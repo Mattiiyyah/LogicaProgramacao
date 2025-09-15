c346ba5ff7892577ef2a6a873a408862ae0bf01f
Matheus de Paula Santos <matheus1516paula@gmail.com>
2025-09-15 19:47:22 -0300

    Update CalculaSalario.java

--- src/CalculaSalario.java
+++ src/CalculaSalario.java
@@ -1,14 +1,14 @@
 import java.util.Scanner;
 public class CalculaSalario {
     public static void main(String args[]) {
-        final double valorHora = 3.0, inss = 0.08;
+        final double VALORHORA = 3.0, INSS = 0.08;
         int horasTrabalhadas;
         double salarioBruto, desconto, salarioLiquido;
         Scanner leitor = new Scanner(System.in);
         System.out.print("\nDigite as horas trabalhadas: ");
         horasTrabalhadas = leitor.nextInt();
-        salarioBruto  = valorHora * horasTrabalhadas;
-        desconto = salarioBruto * inss;
+        salarioBruto  = VALORHORA * horasTrabalhadas;
+        desconto = salarioBruto * INSS;
         salarioLiquido = salarioBruto - desconto;
         System.out.printf("\nO valor do salário bruto é: %.2f ", salarioBruto);
         System.out.printf("\nO valor o desconto é: %.2f", desconto);
