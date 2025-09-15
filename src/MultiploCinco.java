76eee4580ddf72203f6c217d5b37d3c64a80dcc2
Matheus de Paula Santos <matheus1516paula@gmail.com>
2025-09-15 20:23:55 -0300

    Update MultiploCinco.java

--- src/MultiploCinco.java
+++ src/MultiploCinco.java
@@ -3,14 +3,14 @@ import java.util.Scanner;
 public class MultiploCinco {
     public static void main (String args[]) {
         Scanner leitor = new Scanner(System.in);
-        final int multiplo;
+        final int MULTIPLO;
         int numero;
         System.out.print("\nDigite um número: ");
         numero = leitor.nextInt();
 
-        multiplo = numero % 5;
+        MULTIPLO = numero % 5;
 
-        if(multiplo == 0) System.out.print("\nO número: " + numero + " é multiplo de 5");
+        if(MULTIPLO == 0) System.out.print("\nO número: " + numero + " é multiplo de 5");
         else System.out.print("\nO número: " + numero + " não é multiplo de 5");
 
         leitor.close();
