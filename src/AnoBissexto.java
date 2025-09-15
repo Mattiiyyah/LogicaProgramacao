63ccdcb4038de8349fac77942f7926a2a1ec5820
Matheus de Paula Santos <matheus1516paula@gmail.com>
2025-09-15 19:47:18 -0300

    Update AnoBissexto.java

--- src/AnoBissexto.java
+++ src/AnoBissexto.java
@@ -2,20 +2,15 @@ import java.util.Scanner;
 public class AnoBissexto {
     public static void main(String[] args) {
         Scanner leitor = new Scanner(System.in);
-        final int bissexto = 366, ano = 365;
+        final int BISSEXTO = 366, ANO = 365;
         int dias;
         System.out.println("\n---- ##VERIFICANDO ANO BISSEXTO## ----");
         System.out.print("Digite a quantidades de dias que tem o ano: ");
         dias = leitor.nextInt();
 
-        if(dias == bissexto) {
-            System.out.print("\nO ano é bissexto: " + dias + " dias");
-        }
-        else if ( dias != ano) {
-            System.out.print("\nDigite a quantidade dos dias do ano corretamente: " + dias + " dias");
-        } else {
-            System.out.print("\nQue pena o ano não é bissexto: " + dias + " dias");
-        }
+        if(dias == BISSEXTO) System.out.print("\nO ano é bissexto: " + dias + " dias");
+        else if ( dias != ANO) System.out.print("\nDigite a quantidade dos dias do ano corretamente: " + dias + " dias");
+        else System.out.print("\nQue pena o ano não é bissexto: " + dias + " dias");
 
         leitor.close();
         System.exit(0);
