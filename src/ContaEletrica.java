a77d003b23e07ea00dad003e01ce73acd642b6b8
Matheus de Paula Santos <matheus1516paula@gmail.com>
2025-09-15 20:02:55 -0300

    Update ContaEletrica.java

--- src/ContaEletrica.java
+++ src/ContaEletrica.java
@@ -3,17 +3,18 @@ import java.util.Scanner;
 public class ContaEletrica {
     public static void main(String[] args) {
         Scanner leitor = new Scanner(System.in);
-        final double supe_khw = 100, valor_khw = 0.85, adicao = 0.10, desconto = 0.15;
+        final int LIMITE = 100;
+        final double VALOR_KHW = 0.85, ADICAO = 0.10, DESCONTO = 0.15;
         double con_khw, valor_consumido, valor, valor_desconto;
         System.out.print("\n Digite o valor consumido em KWH: ");
         con_khw = leitor.nextDouble();
-        valor_consumido = con_khw * valor_khw;
+        valor_consumido = con_khw * VALOR_KHW;
 
-        if(con_khw >= supe_khw) {
-            valor = valor_consumido + (valor_consumido * adicao);
+        if(con_khw > LIMITE) {
+            valor = valor_consumido + (valor_consumido * ADICAO);
             System.out.printf("\nO valor (com acréscimo de 10%%) em R$ consumido por KWH é: %.2f", valor);
         }  else {
-            valor_desconto = valor_consumido * desconto;
+            valor_desconto = valor_consumido * DESCONTO;
             valor = valor_consumido - valor_desconto;
             System.out.printf("\nO valor (com desconto de 15%%) em R$ consumido por KWH é: %.2f", valor);
         }
