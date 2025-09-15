648856f683a5ef20b779563e3076800707f769ff
Matheus de Paula Santos <matheus1516paula@gmail.com>
2025-09-15 20:26:51 -0300

    Update ParImpar.java

--- src/ParImpar.java
+++ src/ParImpar.java
@@ -2,18 +2,15 @@ import java.util.Scanner;
 
 public class ParImpar {
     public static void main(String args[]) {
-        final int resto;
+        final int RESTO, VERIFICACAO = 0;
         int n;
         Scanner leitor = new Scanner(System.in);
         System.out.print("\nDigite um número: ");
         n = leitor.nextInt();
-        resto = n % 2;
+        RESTO = n % 2;
 
-        if(resto == 0) {
-            System.out.println("\nO número é par: " + n);
-        } else {
-            System.out.println("\nO número é ímpar: " + n);
-        }
+        if(RESTO == VERIFICACAO) System.out.println("\nO número é par: " + n);
+        else System.out.println("\nO número é ímpar: " + n);
 
         leitor.close();
         System.exit(0);
