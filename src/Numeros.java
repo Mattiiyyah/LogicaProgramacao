f408acec67a3def729f5055ba6699818cd84a3a8
Matheus de Paula Santos <matheus1516paula@gmail.com>
2025-09-15 20:25:05 -0300

    Update Numeros.java

--- src/Numeros.java
+++ src/Numeros.java
@@ -2,18 +2,16 @@ import java.util.Scanner;
 public class Numeros {
     public static void main(String[] args) {
         Scanner leitor = new Scanner(System.in);
+        final int VERIFICACAO = 0;
         int numero;
         System.out.println("\n --- ##VERIFICANDNO NÚMERO## ---");
         System.out.print("\n Digite um número qualquer: ");
         numero = leitor.nextInt();
 
-        if(numero > 0) {
-            System.out.print("\n O número: " + numero + " é positivo");
-        } else if (numero < 0 ) {
-            System.out.print("\n O número: " + numero + " é negativo");
-        } else {
-            System.out.print("\n O número: " + numero + " é zero");
-        }
+        if(numero > VERIFICACAO) System.out.print("\n O número: " + numero + " é positivo");
+        else if (numero < VERIFICACAO ) System.out.print("\n O número: " + numero + " é negativo");
+        else System.out.print("\n O número: " + numero + " é zero");
+
 
         leitor.close();
         System.exit(0);
