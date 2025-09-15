296bbdc3c10633525780d5f2b9e24d55cff61267
Matheus de Paula Santos <matheus1516paula@gmail.com>
2025-09-15 19:57:21 -0300

    Update CaracteristicaPessoa.java

--- src/CaracteristicaPessoa.java
+++ src/CaracteristicaPessoa.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
 public class CaracteristicaPessoa {
     public static void main(String[] args) {
         Scanner leitor = new Scanner(System.in);
+        final int IDADE1 = 18, IDADE2 = 68;
+        final double ALTURA1 = 1.65,ALTURA2 = 1.75, PESO1 = 70, PESO2 = 80;
         int idade;
         double altura, peso;
         System.out.println("\n--- ## VERIFICANDO CARACTERISTICAS ## ---");
@@ -12,21 +14,21 @@ public class CaracteristicaPessoa {
         System.out.print("\nDigite seu peso: ");
         peso = leitor.nextDouble();
 
-        if((idade > 18 && idade < 68) && (altura > 1.65 && altura < 1.75) && (peso > 70 && peso < 80)) {
+        if((idade > IDADE1 && idade < IDADE2) && (altura > ALTURA1 && altura < ALTURA2) && (peso > PESO1 && peso < PESO2)) {
                     System.out.println("\nVOCÊ POSSUI TODAS AS CARACTERISTICAS DESEJADAS:");
                     System.out.print("\n" + idade + " anos " + altura + " m " + peso + " kg ");
         } else {
             System.out.println("\nVocê não possui as caracteristicas desejadas pois: ");
 
-            if(idade <= 18 || idade >= 68) {
+            if(idade <= IDADE1 || idade >= IDADE2) {
                 System.out.print("\nSua idade é: " + idade + " anos");
             }
 
-            if(altura < 1.65 || altura > 1.75) {
+            if(altura < ALTURA1 || altura > ALTURA2) {
                 System.out.print("\nSua altura é: " + altura + " m");
             }
 
-            if(peso < 70 || peso > 80) {
+            if(peso < PESO1 || peso > PESO2) {
                 System.out.print("\nSeu peso é: " + peso + " kg");
             }
         }
