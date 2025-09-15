5b5e40bb495cc1279681246d288c69116dcc820d
Matheus de Paula Santos <matheus1516paula@gmail.com>
2025-09-15 20:06:49 -0300

    Update ContaEnergia.java

--- src/ContaEnergia.java
+++ src/ContaEnergia.java
@@ -3,14 +3,15 @@ import java.util.Scanner;
 public class ContaEnergia {
     public static void main(String args[]) {
         Scanner leitor = new Scanner(System.in);
-        final double kwh = 100, valorKwh = 1.50, porcentual = 0.10;
+        final int KWH = 100;
+        final double VALOR_KHW = 1.50, PORCENTUAL = 0.10;
         double quan_Consumida, valor;
         System.out.print("\nDigite a quantidade de Khh consumida: ");
         quan_Consumida = leitor.nextDouble();
-        valor = quan_Consumida * valorKwh;
+        valor = quan_Consumida * VALOR_KHW;
 
-        if(quan_Consumida > kwh) {
-            valor *= porcentual;
+        if(quan_Consumida > KWH) {
+            valor *= PORCENTUAL;
             System.out.printf("\nO valor pago com adição é: %.2f", valor);
         } else {
             System.out.printf("\nO valor pago é:  %.2f",valor);
