54c54620b3d4a99dfcb28c46b4dbb30667861f74
Matheus de Paula Santos <matheus1516paula@gmail.com>
2025-09-15 20:34:24 -0300

    Update ValorAgua.java

--- src/ValorAgua.java
+++ src/ValorAgua.java
@@ -3,18 +3,16 @@ import java.util.Scanner;
 public class ValorAgua {
    public static void main(String args[]) {
        Scanner leitor = new Scanner(System.in);
-       final double valor1 = 1.20, valor2 = 1.50;
+       final int LIMITE = 10;
+       final double V1 = 1.20, V2 = 1.50;
        double quan_Consumida, valorConsumido;
        System.out.print("\nDigite o valor de água consumido em metros cubicos: ");
        quan_Consumida = leitor.nextDouble();
 
-       if (quan_Consumida > 0 && quan_Consumida <= 10) {
-          valorConsumido = quan_Consumida * valor1;
-          System.out.println("\nO valor consumido em metros cubicos é: " + valorConsumido);
-       } else {
-           valorConsumido = quan_Consumida * valor2;
-           System.out.println("\nO valor consumido em metros cubicos é: " + valorConsumido);
-       }
+       if (quan_Consumida <= LIMITE) valorConsumido = quan_Consumida * V1;
+       else valorConsumido = quan_Consumida * V2;
+
+       System.out.println("\nO valor consumido em metros cubicos é: " + valorConsumido);
        leitor.close();
        System.exit(0);
 
