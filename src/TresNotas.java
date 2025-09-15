747730c23049ccbacbe501cfa6edbca175751a1c
Matheus de Paula Santos <matheus1516paula@gmail.com>
2025-09-15 20:32:23 -0300

    Update TresNotas.java

--- src/TresNotas.java
+++ src/TresNotas.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 public class TresNotas {
     public static void main(String args[]) {
-        final double p1 = 0.5, p2 = 0.3, p3 = 0.2;
+        final double P1 = 0.5, P2 = 0.3, P3 = 0.2;
         double n1, n2, n3, m;
         Scanner leitor = new Scanner(System.in);
         System.out.print("\nDigite o valor da primeira nota: ");
@@ -10,7 +10,7 @@ public class TresNotas {
         n2 = leitor.nextDouble();
         System.out.print("\nDigite o valor da terceira nota: ");
         n3 = leitor.nextDouble();
-        m = (n1 * p1) + (n2 * p2) + (n3 * p3);
+        m = (n1 * P1) + (n2 * P2) + (n3 * P3);
         System.out.println("\nO valor da média da nota é: " + m);
 
         leitor.close();
