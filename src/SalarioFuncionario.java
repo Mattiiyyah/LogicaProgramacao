8e147873ced815a64355a2471a72de4406e4094c
Matheus de Paula Santos <matheus1516paula@gmail.com>
2025-09-15 20:31:33 -0300

    Update SalarioFuncionario.java

--- src/SalarioFuncionario.java
+++ src/SalarioFuncionario.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class SalarioFuncionario {
     public static void main(String[] args) {
         Scanner leitor = new Scanner(System.in);
-        final double  fgts = 0.08, inss = 0.11, acrescimo = 0.2;
+        final double  FGTS = 0.08, INSS = 0.11, ACRESCIMO = 0.2;
         String cargo;
         double salario_bruto, salario_liquido, valor_fgts, valor_inss, salario_bonus;
         System.out.println("------------------------------");
@@ -14,8 +14,8 @@ public class SalarioFuncionario {
         System.out.print("\nDigite o salário bruto: ");
         salario_bruto = leitor.nextDouble();
 
-        valor_fgts = salario_bruto * fgts;
-        valor_inss = salario_bruto * inss;
+        valor_fgts = salario_bruto * FGTS;
+        valor_inss = salario_bruto * INSS;
         salario_liquido = salario_bruto - valor_inss;
 
         System.out.printf("Salário bruto: %.2f%n", salario_bruto);
@@ -24,7 +24,7 @@ public class SalarioFuncionario {
         System.out.printf("Salário líquido: %.2f%n", salario_liquido);
 
         if(cargo.equalsIgnoreCase("Professor") || cargo.equalsIgnoreCase("professor")) {
-            salario_bonus = salario_liquido + (salario_liquido * acrescimo);
+            salario_bonus = salario_liquido + (salario_liquido * ACRESCIMO);
             System.out.println("\nO valor bônus para professor é: " + salario_bonus);
         } else {
             System.out.println("\nSem bônus cargos diferentes");
