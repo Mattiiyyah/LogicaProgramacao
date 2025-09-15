2bd190bdc886566088fb8c9fde8d7b1159b1ceff
Matheus de Paula Santos <matheus1516paula@gmail.com>
2025-09-15 20:21:37 -0300

    Update EnergiaCasa.java

--- src/EnergiaCasa.java
+++ src/EnergiaCasa.java
@@ -2,21 +2,21 @@ import java.util.Scanner;
 public class EnergiaCasa {
     public static void main(String[] args) {
         Scanner leitor = new Scanner(System.in);
-        final double v1 = 1.0, v2 = 1.3, v3 = 2.0;
+        final int LIM_1 = 50, LIM_2 = 100;
+        final double V1 = 1.0, V2 = 1.3, V3 = 2.0;
         double consumo_khw, valorTotal = 0;
         System.out.println("-- ## CALCULANDO O VALOR DE ENERGIA NA CASA ## --");
         System.out.print("\nDigite a quantidade consumida em KHz: ");
         consumo_khw = leitor.nextDouble();
 
-        if (consumo_khw > 0 && consumo_khw <= 50) {
-            valorTotal = consumo_khw * v1;
-        } else if (consumo_khw >= 51 && consumo_khw <= 100) {
-            valorTotal = (50 * v1) + (consumo_khw - 50) * v2;
-        } else {
-            valorTotal = (50 * v1) + (50 * v2) + (consumo_khw - 100) * v3;
-        }
+        if (consumo_khw <= LIM_1) valorTotal = consumo_khw * V1;
+        else if (consumo_khw <= LIM_2) valorTotal = (LIM_1 * V1) + (consumo_khw - LIM_1) * V2;
+        else valorTotal = (LIM_1 * V1) + (LIM_1 * V2) + (consumo_khw - LIM_2) * V3;
 
         System.out.print("\nO valor total é: " + valorTotal);
+
+        leitor.close();
+        System.exit(0);
 
     }
 }
