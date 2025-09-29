6fbbd70fb876270de9cbc61d42db0429cf62c331
Matheus de Paula Santos <matheus1516paula@gmail.com>
2025-09-29 11:00:33 -0300

    Update ProgX.java

--- src/ProgX.java
+++ src/ProgX.java
@@ -7,6 +7,8 @@ public class ProgX {
         final int LOOP = 5;
         double salario, soma = 0;
         int idade, somaIdade = 0;
+        char sexo; //apenas um caracter masculino ou feminino
+        int contF = 0, contM = 0;
 
         for (int i = 1; i <= 5; i++) {
             System.out.println("Salario " + i + ": ");
@@ -15,12 +17,22 @@ public class ProgX {
             System.out.println("Informe a idade: ");
             idade = leitor.nextInt();
 
+            System.out.println("Informe o sexo: [F] - Feminino ou [M] - Masculino: ");
+            sexo = leitor.next().charAt(0);
+
             soma += salario;
             somaIdade += idade;
+
+            if(sexo == 'f' || sexo == 'F')
+                contF++;
+            else
+                contM++;
         }
 
         System.out.print("\nMédia Salarial: " + soma / LOOP);
         System.out.print("\nMédia das idades: " + somaIdade / LOOP);
+        System.out.print("\nQuantidade de mulheres: " + contF);
+        System.out.print("\nQuantidade de homens: " + contM);
 
         leitor.close();
         System.exit(0);
